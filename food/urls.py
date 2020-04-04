37bc0631ce64bfd83a58aa003526d93305c491a3
Jefferson <jeffarrubla@gmail.com>
2020-04-04 17:16:30 +0000

    Creating the detail view

--- food/urls.py
+++ food/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 
 
 urlpatterns = [
+	#/food/
 	path('', views.index, name='index'),
-	path('item/', views.item, name='item'),
+	#/food/1
+	path('<int:item_id>/',views.detail,name='detail'),
+	#/item/
+	path('item/', views.item, name='item'),	
 ]
