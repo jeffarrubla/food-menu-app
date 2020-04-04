7da320e2730041efcacc61d291ad9acb855c71ef
Jefferson <jeffarrubla@gmail.com>
2020-04-04 20:36:31 +0000

    Adding image field to model

--- food/models.py
+++ food/models.py
@@ -9,3 +9,4 @@ class Item(models.Model):
 	item_name = models.CharField(max_length=200)
 	item_desc = models.CharField(max_length=200)
 	item_price = models.IntegerField()
+	item_image = models.CharField(max_length=500,default="https://cdn.dribbble.com/users/1012566/screenshots/4187820/topic-2.jpg")
